463032c8aea13dc2b76fc01a3d9fa973208f0193
MeetHansalia <meethansalia99@gmail.com>
2023-11-08 17:56:15 +0530

    initial

---
+++ dummy-data.js
@@ -0,0 +1,85 @@
+const DUMMY_EVENTS = [
+    {
+      id: 'e1',
+      title: 'Programming for everyone',
+      description:
+        'next.js seminar on the overview of the framwork',
+      location: 'RingRoad , 12345 street',
+      date: '2021-05-12',
+      image: 'images/image.jpg',
+      isFeatured: false,
+    },
+    {
+      id: 'e2',
+      title: 'Networking for introverts',
+      description:
+        "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
+      location: 'New Wall Street 5, 98765 New Work',
+      date: '2021-05-30',
+      image: 'images/image.jpg',
+      isFeatured: true,
+    },
+    {
+      id: 'e3',
+      title: 'Networking for extroverts',
+      description:
+        'You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.',
+      location: 'My Street 12, 10115 Broke City',
+      date: '2022-05-08',
+      image: 'images/image.jpg',
+      isFeatured: true,
+    },
+    {
+      id: 'e4',
+      title: 'Networking for extroverts',
+      description:
+        'You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.',
+      location: 'My Street 12, 10115 Broke City',
+      date: '2021-06-09',
+      image: 'images/image.jpg',
+      isFeatured: true,
+    },
+    {
+      id: 'e5',
+      title: 'Networking for extroverts',
+      description:
+        'You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.',
+      location: 'My Street 12, 10115 Broke City',
+      date: '2022-03-04',
+      image: 'images/image.jpg',
+      isFeatured: true,
+    },
+    {
+      id: 'e6',
+      title: 'Networking for extroverts',
+      description:
+        'You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.',
+      location: 'My Street 12, 10115 Broke City',
+      date: '2021-02-02',
+      image: 'images/image.jpg',
+      isFeatured: true,
+    },
+  ];
+  
+  export function getFeaturedEvents() {
+    return DUMMY_EVENTS.filter((event) => event.isFeatured);
+  }
+  
+  export function getAllEvents() {
+    return DUMMY_EVENTS;
+  }
+  
+  export function getFilteredEvents(dateFilter) {
+    const { year, month } = dateFilter;
+  
+    let filteredEvents = DUMMY_EVENTS.filter((event) => {
+      const eventDate = new Date(event.date);
+      return eventDate.getFullYear() === year && eventDate.getMonth() === month - 1;
+    });
+  
+    return filteredEvents;
+  }
+  
+  export function getEventById(id) {
+    return DUMMY_EVENTS.find((event) => event.id === id);
+  }
